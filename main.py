7513d7486bcc87d0c5b0b6a71d6b555792f88ff6
Parth Shah <87560178+codingis4noobs2@users.noreply.github.com>
2024-10-22 13:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import cloudinary
 from cloudinary import CloudinaryImage
 import cloudinary.uploader
 import cloudinary.api
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    cloud_name=st.secrets["CLOUDINARY_CLOUD_NAME"],
+    api_key=st.secrets["CLOUDINARY_API_KEY"],
+    api_secret=st.secrets["CLOUDINARY_API_SECRET"],
     secure=True
 )
 
